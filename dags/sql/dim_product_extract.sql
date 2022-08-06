0c8e704eba39a2d3c296fc36cdfeb00f2c10b3df
Pinky Gautam <pkgtm1998@gmail.com>
2022-08-06 23:57:14 +0700

    remove unused files

--- dags/sql/dim_product_extract.sql
+++
@@ -1,5 +0,0 @@
-TRUNCATE TABLE staging.dim_product;
-
-INSERT INTO staging.dim_product (product_id, product_name, category, subcategory)
-SELECT product_id, product_name, category, subcategory
-FROM public.products;
--- setup.sh
+++
@@ -1,7 +0,0 @@
-source ${PWD}/venv/bin/activate
-
-mkdir -p ${PWD}/dags/
-
-export AIRFLOW_HOME=~/airflow
-export AIRFLOW__CORE__DAGS_FOLDER="${PWD}/dags"
-export PYTHONPATH=${PWD}
